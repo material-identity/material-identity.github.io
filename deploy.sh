09a487f1bd7635da4ec1f42a3dc7432aa672a5d9
Hannes Stiebitzhofer <hannes@stiebitzhofer.com>
2022-09-10 22:29:38 +0200

    fix: bug in deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ cd docs/.vuepress/dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:material-identity/material-identity.github.io master:github-pages
+git push -f git@github.com:material-identity/material-identity.github.io main:github-pages
 cd -
