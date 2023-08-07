9092c0784f44f9cafceed788866e7dc23f06b27d
Hannes Stiebitzhofer <581724+stiebitzhofer@users.noreply.github.com>
2023-08-07 22:14:33 +0300

    Delete deploy script obsolete by Github Action

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-set -e
-npm run docs:build
-cd docs/.vuepress/dist
-git init
-git add -A
-git commit -m 'deploy'
-git push -f git@github.com:material-identity/material-identity.github.io main:github-pages
-cd -
